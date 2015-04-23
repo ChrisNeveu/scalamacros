25079296d650c0bc98ac8f1d8b893c0c7f47eb81
Chris Neveu <mail@chrisneveu.com>
2015-04-22 20:26:08 -0400

    Enum.

--- annotationTest/src/test/scala/TestAnnotationSpec.scala
+++ annotationTest/src/test/scala/TestAnnotationSpec.scala
@@ -2,3 +2,17 @@ package com.chrisneveu
 
 @toMap
 case class Foo(a : String, b : Int)
+
+@enum
+class Bar {
+   Red
+   Blue
+   Black
+}
+object Bar {
+   def stringify(b : Bar) = b match {
+      case Red   ⇒ "red"
+      case Blue  ⇒ "blue"
+      case Black ⇒ "black"
+   }
+}
